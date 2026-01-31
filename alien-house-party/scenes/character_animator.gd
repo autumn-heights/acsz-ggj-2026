8f35e3848c056eaa560a89a0d0b92a0d22b656c7
Eevaio <combinationeva@gmail.com>
2026-01-31 14:27:25 +0800

    da dolls

--- alien-house-party/scenes/character_animator.gd
+++ alien-house-party/scenes/character_animator.gd
@@ -1,6 +1,7 @@
-extends Node
-enum Gamestates {IDLE, MOVING, DIALOGUE}
-var current_state : Gamestates
+extends Node2D
+
+var characterTween : Tween
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func pop_character():
+	var ctr = get_viewport_rect().get_center()
+	

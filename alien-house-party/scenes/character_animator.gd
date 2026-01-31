d56a45a956fbaf390b9070878ef9028897260919
Eevaio <combinationeva@gmail.com>
2026-01-31 14:48:17 +0800

    dialogue animator sommit

--- alien-house-party/scenes/character_animator.gd
+++ alien-house-party/scenes/character_animator.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var characterTween : Tween
+@onready var sprite_node = $CharacterSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,6 +12,27 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func pop_character():
-	var ctr = get_viewport_rect().get_center()
-	
+func animate_character(showing):
+	if characterTween:
+		characterTween.kill() ## pauses existing running animator
+	characterTween = create_tween().set_trans(Tween.TRANS_CUBIC)
+	if showing:
+		characterTween.set_trans(Tween.TRANS_BOUNCE)
+		var characterHeight = sprite_node.get_rect().size.y
+		var s = get_viewport_rect().size
+		var p = Vector2()
+		p.x = s.x / 2 ## getting the center of the viewport
+		p.y = s.y + characterHeight
+		p += get_viewport_rect().position
+		var d = p ## setting sprite destination
+		d.y -= characterHeight *1.5
+		sprite_node.position = p
+		sprite_node.modulate = Color.WHITE
+		sprite_node.show()
+		characterTween.tween_property(sprite_node, "position", d, 1.2)
+	else:
+		characterTween.set_ease(Tween.EASE_OUT)
+		characterTween.tween_property(sprite_node, "modulate",
+		 Color(1, 1, 1, 0), 3.0)
+		characterTween.tween_callback(sprite_node.hide)
+	characterTween.tween_callback(characterTween.kill)

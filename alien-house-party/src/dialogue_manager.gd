4b963ee28d3af6d91a1efafd0bec04bb305caa46
autumn-brough <78739123+autumn-heights@users.noreply.github.com>
2026-02-01 09:47:00 +0800

    character portraits working

--- alien-house-party/src/dialogue_manager.gd
+++ alien-house-party/src/dialogue_manager.gd
@@ -12,6 +12,8 @@ var num_lines_in_current_script: int = -1;
 @onready var npc_portrait = %NPCPortrait
 
 const npc_textures = {
+	"PLAYER_CHARACTER":
+		preload("res://assets/attendees/aftonsparv.png"),
 	"attendees_sprite_crushed1.png":
 		preload("res://assets/attendees/attendees_sprite_crushed1.png"),
 	"attendees_sprite_crushed2.png":
@@ -43,39 +45,51 @@ func load_json_file(path: String) -> void:
 
 func trigger_new_dialogue(script_id: String):
 	
+	if script_id != null and not script_id in json_data.keys():
+		assert(false) # incorrect script_id triggered!
+	
 	if json_data[script_id]["type"] == "talking":
 		current_script_id = script_id;
 		current_line_id = -1
 		dialogue_box.visible = true;
 		num_lines_in_current_script = len(json_data[script_id]["lines"])
-		try_next_dialogue_line()
+		trigger_next_dialogue_line()
 	
 	elif json_data[script_id]["type"] == "choice":
-		pass
-		# TODO: display the choice options on the screen
+		dialogue_box.visible = false;
+		current_script_id = script_id;
+		display_choice(script_id)
 
-func try_next_dialogue_line():
+func trigger_next_dialogue_line():
 	# if there's another line of dialogue to display, then do that
 	# if there's not another line of dialogue to display, then close the dialogue box
 	if current_line_id < num_lines_in_current_script -1 :
 		current_line_id += 1
 		display_text(json_data[current_script_id]["lines"][current_line_id]["text"])
+		print(json_data[current_script_id]["lines"][current_line_id]["speaker"])
+		display_npc_portrait(json_data[current_script_id]["lines"][current_line_id]["speaker"])
 	else:
+		
 		pass
 		# TODO: Handle end of dialogue, trigger next step per jsonscript
 
 func _on_advance_dialogue():
 	# FUNCTION THAT GETS TRIGGERED WHEN THE PLAYER ADVANCES THE DIALOGUE BOX
-	try_next_dialogue_line()
+	trigger_next_dialogue_line()
 
 
-func display_text(data: String):
-	dialogue_box.set_new_text(data);
+func display_text(text: String):
+	dialogue_box.set_new_text(text);
 	
-func display_npc_portrait(data: String):
-	npc_portrait.texture = npc_textures[data]
+func display_npc_portrait(npc_texture_key: String):
+	if npc_texture_key in npc_textures.keys():
+		npc_portrait.visible = true
+		npc_portrait.texture = npc_textures[npc_texture_key]
+	else:
+		npc_portrait.visible = false
 
-func display_choice(data):
+func display_choice(script_id):
+	var data = json_data[script_id]
 	for child in choice_container.get_children():
 		child.queue_free()
 	

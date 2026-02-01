b6f4a1b4c2d40bb88e556e399518105d17787d82
autumn-brough <78739123+autumn-heights@users.noreply.github.com>
2026-02-01 10:35:10 +0800

    Added navigation buttons

--- alien-house-party/src/dialogue_manager.gd
+++ alien-house-party/src/dialogue_manager.gd
@@ -1,5 +1,9 @@
 extends Node
 
+signal pressed_go_straight
+signal pressed_go_left
+signal pressed_go_right
+
 var json_data: Dictionary
 var console_label: Label
 var choice_container: VBoxContainer
@@ -8,8 +12,12 @@ var current_script_id: String
 var current_line_id: int = -1;
 var num_lines_in_current_script: int = -1;
 
+var movement_cooldown = 0;
+var movement_cooldown_max = 0.5; # in seconds
+
 @onready var dialogue_box = %DialogueBox
 @onready var npc_portrait = %NPCPortrait
+@onready var nav_buttons = %NavigationButtons
 
 const npc_textures = {
 	"PLAYER_CHARACTER":
@@ -52,11 +60,13 @@ func trigger_new_dialogue(script_id: String):
 		current_script_id = script_id;
 		current_line_id = -1
 		dialogue_box.visible = true;
+		nav_buttons.visible = false;
 		num_lines_in_current_script = len(json_data[script_id]["lines"])
 		trigger_next_dialogue_line()
 	
 	elif json_data[script_id]["type"] == "choice":
 		dialogue_box.visible = false;
+		nav_buttons.visible = false;
 		current_script_id = script_id;
 		display_choice(script_id)
 
@@ -68,10 +78,13 @@ func trigger_next_dialogue_line():
 		display_text(json_data[current_script_id]["lines"][current_line_id]["text"])
 		print(json_data[current_script_id]["lines"][current_line_id]["speaker"])
 		display_npc_portrait(json_data[current_script_id]["lines"][current_line_id]["speaker"])
+	elif json_data[current_script_id]["next"] != null:
+		dialogue_box.visible = false;
+		trigger_new_dialogue(json_data[current_script_id]["next"])
 	else:
-		
+		dialogue_box.visible = false;
+		nav_buttons.visible = true;
 		pass
-		# TODO: Handle end of dialogue, trigger next step per jsonscript
 
 func _on_advance_dialogue():
 	# FUNCTION THAT GETS TRIGGERED WHEN THE PLAYER ADVANCES THE DIALOGUE BOX
@@ -144,8 +157,31 @@ func _on_choice_pressed(choice_data):
 
 
 func _process(delta: float) -> void:
+	movement_cooldown -= delta;
+	if Input.is_action_just_pressed("up"):
+		_try_send_movement_signal(pressed_go_straight)
+	elif Input.is_action_just_pressed("left"):
+		_try_send_movement_signal(pressed_go_left)
+	elif Input.is_action_just_pressed("right"):
+		_try_send_movement_signal(pressed_go_right)
+	
 	pass
 
 
+func _try_send_movement_signal(my_signal):
+	if movement_cooldown <= 0 and nav_buttons.visible:
+		my_signal.emit()
+		movement_cooldown = movement_cooldown_max
+		print(my_signal)
+
 func _on_debug_convo_button_pressed() -> void:
 	trigger_new_dialogue("dialogue_1");
+
+func _on_go_left_button_pressed() -> void:
+	_try_send_movement_signal(pressed_go_left)
+
+func _on_go_straight_button_pressed() -> void:
+	_try_send_movement_signal(pressed_go_straight)
+
+func _on_go_right_button_pressed() -> void:
+	_try_send_movement_signal(pressed_go_right)
